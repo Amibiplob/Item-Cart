5917396a3d656414d354c82439771536081350fd
AmiBiplob <muhammadbiplob7@gmail.com>
2026-02-17 19:06:32 +0600

    modal added

--- app.js
+++ app.js
@@ -24,53 +24,47 @@ let showCategories = (categories) => {
 };
 let showProduct = (products) => {
   // console.log(product);
+  let loadProduct = document.getElementById("loadProduct");
   loadProduct.textContent = "";
   for (const product of products) {
     //  console.log(product);
-    let { title, price, description, category, image, rating } = product;
-    let loadProduct = document.getElementById("loadProduct");
+    let { title, price, description, category, image, rating, id } = product;
 
     let div = document.createElement("div");
     div.innerHTML = `
-         <div class="card bg-base-100 shadow-md">
-          <figure class="bg-gray-100">
-            <img
-              src=${image}
-              class=""
-            />
-          </figure>
+  <div class="card bg-base-100 shadow-md">
+    <figure class="bg-gray-100">
+      <img src="${image}" class="h-[300px]" />
+    </figure>
 
-          <div class="card-body">
-            <div class="flex justify-between items-center">
-              <span class="badge badge-soft badge-primary">
-                ${category}
-              </span>
-              <span class="text-sm"
-                ><i
-                  class="fa-solid fa-star"
-                  style="color: rgba(255, 212, 59, 1)"
-                ></i>
-                <span>${rating.rate}(${rating.count})</span></span
-              >
-            </div>
+    <div class="card-body h-[200px] md:h-[250px]">
+      <div class="flex justify-between items-center">
+        <span class="badge badge-soft badge-primary"> ${category} </span>
+        <span class="text-sm"
+          ><i class="fa-solid fa-star" style="color: rgba(255, 212, 59, 1)"></i>
+          <span>${rating.rate}(${rating.count})</span></span
+        >
+      </div>
 
-            <h2 class="card-title text-base font-semibold">
-             ${title}
-            </h2>
+      <h2 class="card-title text-base font-semibold">${title}</h2>
 
-            <p class="text-lg font-bold">$${price}</p>
+      <p class="text-lg font-bold">$${price}</p>
 
-            <div class="flex gap-4">
-              <button class="btn btn-outline btn-sm flex-1">
-                <i class="fa-regular fa-eye"></i>
-                Details
-              </button>
-              <button class="btn btn-primary btn-sm flex-1">
-                <i class="fa-solid fa-cart-shopping"></i> Add
-              </button>
-            </div>
-          </div>
-        </div>
+      <div class="flex gap-4">
+        <label
+          onclick="modalView(${id})"
+          for="my_modal_6"
+          class="btn btn-outline btn-sm flex-1"
+        >
+          <i class="fa-regular fa-eye"></i>
+          Details
+        </label>
+        <button class="btn btn-primary btn-sm flex-1">
+          <i class="fa-solid fa-cart-shopping"></i> Add
+        </button>
+      </div>
+    </div>
+  </div>
         `;
     loadProduct.appendChild(div);
   }
@@ -83,56 +77,88 @@ let loadProductByCategory = (category) => {
     .then((data) => productByCategory(data));
 };
 let productByCategory = (products) => {
+  let loadProduct = document.getElementById("loadProduct");
   loadProduct.textContent = "";
   for (const product of products) {
     //  console.log(product);
-    let { title, price, description, category, image, rating } = product;
-    let loadProduct = document.getElementById("loadProduct");
+    let { title, price, description, category, image, rating, id } = product;
 
     let div = document.createElement("div");
     div.innerHTML = `
-         <div class="card bg-base-100 shadow-md">
-          <figure class="bg-gray-100">
-            <img
-              src=${image}
-              class=""
-            />
-          </figure>
+  <div class="card bg-base-100 shadow-md">
+    <figure class="bg-gray-100">
+      <img src="${image}" class="h-[300px]" />
+    </figure>
 
-          <div class="card-body">
-            <div class="flex justify-between items-center">
-              <span class="badge badge-soft badge-primary">
-                ${category}
-              </span>
-              <span class="text-sm"
-                ><i
-                  class="fa-solid fa-star"
-                  style="color: rgba(255, 212, 59, 1)"
-                ></i>
-                <span>${rating.rate}(${rating.count})</span></span
-              >
-            </div>
+    <div class="card-body h-[200px] md:h-[250px]">
+      <div class="flex justify-between items-center">
+        <span class="badge badge-soft badge-primary"> ${category} </span>
+        <span class="text-sm"
+          ><i class="fa-solid fa-star" style="color: rgba(255, 212, 59, 1)"></i>
+          <span>${rating.rate}(${rating.count})</span></span
+        >
+      </div>
 
-            <h2 class="card-title text-base font-semibold">
-             ${title}
-            </h2>
+      <h2 class="card-title text-base font-semibold">${title}</h2>
 
-            <p class="text-lg font-bold">$${price}</p>
+      <p class="text-lg font-bold">$${price}</p>
 
-            <div class="flex gap-4">
-              <button class="btn btn-outline btn-sm flex-1">
-                <i class="fa-regular fa-eye"></i>
-                Details
-              </button>
-              <button class="btn btn-primary btn-sm flex-1">
-                <i class="fa-solid fa-cart-shopping"></i> Add
-              </button>
-            </div>
-          </div>
-        </div>
+      <div class="flex gap-4">
+        <label
+          onclick="modalView(${id})"
+          for="my_modal_6"
+          class="btn btn-outline btn-sm flex-1"
+        >
+          <i class="fa-regular fa-eye"></i>
+          Details
+        </label>
+        <button class="btn btn-primary btn-sm flex-1">
+          <i class="fa-solid fa-cart-shopping"></i> Add
+        </button>
+      </div>
+    </div>
+  </div>
         `;
     loadProduct.appendChild(div);
   }
+};
+
+let modalView = (id) => {
+  fetch(`https://fakestoreapi.com/products/${id}`)
+    .then((res) => res.json())
+    .then((data) => showModalProduct(data));
+};
+let showModalProduct = (product) => {
+  console.log(product);
+
+  let { title, price, description, category, image, rating, id } = product;
+  document.getElementById("showModalData").innerHTML = `
+  <div class="card bg-base-100 shadow-md">
+    <figure class="bg-gray-100">
+      <img src="${image}" class="h-[300px]" />
+    </figure>
+    <div class="card-body h-[200px] md:h-[250px] max-w-[700px]">
+      <div class="flex justify-between items-center">
+        <span class="badge badge-soft badge-primary"> ${category} </span>
+        <span class="text-sm"
+          ><i class="fa-solid fa-star" style="color: rgba(255, 212, 59, 1)"></i>
+          <span>${rating.rate}(${rating.count})</span></span
+        >
+      </div>
+      <h2 class="card-title text-base font-semibold">${title}</h2>
+      <div class="">
+      <h2 class="card-title text-lg font-bold">Description :</h2>
+       <span class="text-base font-semibold line-clamp-3">${description}</span></div>
+      <div class="flex items-center">
+      <p class="text-lg font-bold">$${price}</p>
+      <div class="">
+      <label for="my_modal_6" class="btn">Close!</label>
+    </div>
+       </div>
+      </div>
+    </div>
+  </div>
+        `;
 };
 
 loadCategories();
